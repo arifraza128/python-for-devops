177b0d76e69c5516b36aac83322a882816cf5d98
Arif Raza <arif3raza@gmail.com>
2026-02-11 09:39:10 +0530

    first code

---
+++ python/my-first-file.py
@@ -0,0 +1,21 @@
+output = 'yes'
+output = 'no'
+
+a = 100
+b = 200
+env = 'stg'
+
+print(a + b)
+
+# + - * / = (operators
+# a + b (operands)
+# a + b = (operation)
+
+x = 50 
+y = 40
+
+z = x * y
+
+print("multiplication of x and y is", z)
+
+
