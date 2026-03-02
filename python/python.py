efa8f5cf462ed4a5ffa41d466d75f0558aea4a46
Arif Raza <arif3raza@gmail.com>
2026-03-02 20:31:09 +0530

    script

--- python/python.py
+++ python/python.py
@@ -1,1 +1,10 @@
 print('this is my devops working with python')
+
+import psutil
+
+print("CPU Usage:", psutil.cpu_percent(), "%")
+print("Memory Usage:", psutil.virtual_memory().percent, "%")
+print("Disk Usage:", psutil.disk_usage('/').percent, "%")
+
+
+
