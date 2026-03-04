a46da042a89337356724a0a3046f689dc8531512
Arif Raza <arif3raza@gmail.com>
2026-03-04 21:27:31 +0530

    Update python.py

--- python/python.py
+++ python/python.py
@@ -19,3 +19,18 @@ file.write("This file is created by Python for DevOps practice.")
 file.close()
 
 print("File created successfully")
+
+import requests
+
+def check_server(url):
+    try:
+        response = requests.get(url, timeout=5)
+        if response.status_code == 200:
+            print(f" {url} is UP")
+        else:
+            print(f" {url} returned status {response.status_code}")
+    except requests.exceptions.RequestException:
+        print(f"{url} is DOWN")
+
+if __name__ == "__main__":
+    check_server("https://google.com")
