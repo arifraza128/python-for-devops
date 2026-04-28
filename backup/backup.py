1f1bc43465a57aafbbb4aa3abc42b2163cf18982
Arif Raza <arif3raza@gmail.com>
2026-04-28 20:19:06 +0530

    Update backup.py

--- backup/backup.py
+++ backup/backup.py
@@ -1,13 +1,10 @@
 import shutil
 import os
 from datetime import datetime
-
 source = "project_folder"
 backup_dir = "backups"
-
 if not os.path.exists(backup_dir):
     os.mkdir(backup_dir)
-
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 destination = f"{backup_dir}/backup_{timestamp}"
 
