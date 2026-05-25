2f3b0f425529337772bcdeb328440b8e1ef18fd0
Arif Raza <arif3raza@gmail.com>
2026-05-25 21:39:56 +0530

    Add S3 upload functionality to upload_s3.py

--- s3/upload_s3.py
+++ s3/upload_s3.py
@@ -1,1 +1,13 @@
+import boto3
 
+
+s3 = boto3.client('s3')
+
+# Upload file
+s3.upload_file(
+    'test.txt',
+    'my-bucket-name',
+    'uploads/test.txt'
+)
+
+print("File uploaded successfully")
