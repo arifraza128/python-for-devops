4ea2c4ab3f349822178b5e8ae10ca9a0dd6df499
Arif Raza <arif3raza@gmail.com>
2026-05-29 20:29:02 +0530

    Update create_bucket.py

--- s3/create_bucket.py
+++ s3/create_bucket.py
@@ -1,1 +1,12 @@
+import boto3
 
+s3 = boto3.client('s3')
+
+s3.create_bucket(
+    Bucket='my-new-bucket-12345',
+    CreateBucketConfiguration={
+        'LocationConstraint': 'ap-south-1'
+    }
+)
+
+print("Bucket created successfully")
